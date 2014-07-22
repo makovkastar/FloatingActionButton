93b766d93e94bd796a062901d66ec15a8d83c490
makovkastar <makovkastar@gmail.com>
2014-07-22 16:29:12 +0300

    Initial commit

---
+++ sample/src/main/java/com/melnykov/floatingactionbutton/sample/MainActivity.java
@@ -0,0 +1,16 @@
+package com.melnykov.floatingactionbutton.sample;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
