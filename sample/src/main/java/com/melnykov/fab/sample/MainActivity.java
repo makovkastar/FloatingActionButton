414f956810870a4e8f47e2db19bde32ae97360e1
makovkastar <makovkastar@gmail.com>
2014-08-03 22:19:33 +0300

    Simplify sample code

--- sample/src/main/java/com/melnykov/fab/sample/MainActivity.java
+++ sample/src/main/java/com/melnykov/fab/sample/MainActivity.java
@@ -1,6 +1,7 @@
 package com.melnykov.fab.sample;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
@@ -9,19 +10,18 @@ import com.melnykov.fab.FloatingActionButton;
 import com.melnykov.fab.sample.R;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListView listView = (ListView) findViewById(android.R.id.list);
         FloatingActionButton floatingActionButton = (FloatingActionButton) findViewById(R.id.button_floating_action);
-        floatingActionButton.attachToListView(listView);
+        floatingActionButton.attachToListView(getListView());
 
         ListAdapter listAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
                 getResources().getStringArray(R.array.planets));
-        listView.setAdapter(listAdapter);
+        getListView().setAdapter(listAdapter);
     }
 }
