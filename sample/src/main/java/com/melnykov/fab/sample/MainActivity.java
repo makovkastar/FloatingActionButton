b1115b68f2a39de86cfa746f8ae31a4bcfe6b6ee
Thiago Locatelli <thiago.locatelli@gmail.com>
2014-08-07 02:21:09 -0400

    Adding Action button to ScrollView

--- sample/src/main/java/com/melnykov/fab/sample/MainActivity.java
+++ sample/src/main/java/com/melnykov/fab/sample/MainActivity.java
@@ -1,25 +1,31 @@
 package com.melnykov.fab.sample;
 
+import android.app.Activity;
 import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 
 import com.melnykov.fab.FloatingActionButton;
 
 
-public class MainActivity extends ListActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
-        FloatingActionButton floatingActionButton = (FloatingActionButton) findViewById(R.id.button_floating_action);
-        floatingActionButton.attachToListView(getListView());
+    public void scrollview(View view) {
+        Intent intent = new Intent(this, ScrollViewActivity.class);
+        startActivity(intent);
+    }
 
-        ListAdapter listAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
-                getResources().getStringArray(R.array.planets));
-        getListView().setAdapter(listAdapter);
+    public void listview(View view) {
+        Intent intent = new Intent(this, ListViewActivity.class);
+        startActivity(intent);
     }
 }
