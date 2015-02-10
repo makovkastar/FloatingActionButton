a3bfdab13f345d811a4c055ada8836f8c6ca12c4
makovkastar <makovkastar@gmail.com>
2015-02-10 08:49:44 +0200

    Update the sample

--- sample/src/main/java/com/melnykov/fab/sample/MainActivity.java
+++ sample/src/main/java/com/melnykov/fab/sample/MainActivity.java
@@ -19,6 +19,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -139,6 +140,16 @@ public class MainActivity extends ActionBarActivity {
                 @Override
                 public void onScrollUp() {
                     Log.d("ListViewFragment", "onScrollUp()");
+                }
+            }, new AbsListView.OnScrollListener() {
+                @Override
+                public void onScrollStateChanged(AbsListView view, int scrollState) {
+                    Log.d("ListViewFragment", "onScrollStateChanged()");
+                }
+
+                @Override
+                public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+                    Log.d("ListViewFragment", "onScroll()");
                 }
             });
 
